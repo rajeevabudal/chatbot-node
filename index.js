9508a76ca082255c3775ba8afe82a4d367d3a51b
rajeevabudal <rajeevabp92@gmail.com>
2024-03-02 18:14:10 +0530

    removed the api key

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const OpenAI = require("openai");
-const apiKey = "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"
-const openai = new OpenAI({apiKey});
+import OpenAI from "openai";
+
+const openai = new OpenAI({apiKey:process.env.API_KEY});
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant designed to output JSON.",
+      },
+      { role: "user", content: "Who won the world series in 2020?" },
+    ],
+    model: "gpt-3.5-turbo-0125",
+    response_format: { type: "json_object" },
   });
-
-  console.log(completion.choices[0]);
+  console.log(completion.choices[0].message.content);
 }
 
 main();
